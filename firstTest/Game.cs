99983309e3f56e11089a4de463c33382ab74cf2a
trbud1 <trbud1@student.monash.edu>
2016-07-15 09:39:38 +1000

    My init commit

---
+++ firstTest/Game.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace firstTest
+{
+    class Game
+    {
+
+        LinkedList myList = new LinkedList();
+        public  void gameMenu()
+        {
+            string selection;
+            initLinkedList();
+            
+            do
+            {
+                //Clear previous items.
+                Console.Clear();
+
+                //Display contents.
+                myList.PrintNodes();
+                PrintMenu();
+                
+                //Read users input.
+                selection = Console.ReadLine();
+
+                switch (selection)
+                {
+                    //Add Start
+                    case "1":
+                        myList.addNodeStart();
+                        break;
+                    //Add End
+                    case "2":
+                        myList.addNodeEnd(GetName());
+                        break;
+                    //Delete start
+                    case "3":
+                        DeleteAtIndex(0);
+                        //myList.removeNode(0);
+                        break;
+                    //Delete End
+                    case "4":
+                        DeleteAtIndex(myList.size - 1);
+                        //myList.removeNode(myList.size - 1);
+                        break;
+                    //Delete at index
+                    case "5":
+                        DeleteAtIndex(-1);
+                        
+                        break;
+                    case "6":
+                        InsertAtLocation();
+                        
+                        break;
+
+                    default:
+                        break;
+
+                }
+
+            } while (!selection.Equals(99));
+        }
+
+        public void DeleteAtIndex(int i)
+        {
+
+            if (i == -1)
+            {
+                Console.WriteLine("Enter index to delete: ");
+                i = int.Parse(Console.ReadLine());
+                myList.removeNode(i);
+            }
+            else
+            {
+                myList.removeNode(i);
+            }
+            
+        }
+
+        public void InsertAtLocation()
+        {
+            Console.WriteLine("Enter node location: ");
+            int pos = int.Parse(Console.ReadLine());
+            Node newNode = new Node(GetName());
+
+            myList.addNodeAt(pos, newNode);
+        }
+
+        public string GetName()
+        {
+            Console.WriteLine("Enter node name: ");
+            return Console.ReadLine();
+        }
+
+        public void PrintMenu()
+        {
+            Console.WriteLine("1. Insert at start");
+            Console.WriteLine("2. Insert at end");
+            Console.WriteLine("3. Delete at start");
+            Console.WriteLine("4. Delete at end");
+            Console.WriteLine("5. Delete at index");
+            Console.WriteLine("6. Insert at index");
+            Console.Write("Option (99 to exit): ");
+        }
+
+        public void initLinkedList()
+        {
+            string[] items = new string[] { "Sixth", "Fifth", "Fourth", "Third", "Second", "First" };
+
+            foreach (string i in items)
+            {
+                myList.add(i);
+            }
+        }
+
+    }
+}
---
+++ firstTest/Program.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace firstTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            Game g = new Game();
+            g.gameMenu();
+        }
+    }
+
+ 
+
+    public class Node{
+
+        public object value;
+        public Node next;
+
+        public Node(object value)
+        {
+            this.value = value;
+            next = null;
+        }
+
+        
+    }
+
+    public class LinkedList
+    {
+        Node head;
+        public int size = 1;
+
+        public void addNodeEnd(string name)
+        {
+
+            Node newNode = new Node(name);
+            Node currentHead = this.head;
+
+            while (true)
+            {
+                if(currentHead.next == null)
+                {
+                    currentHead.next = newNode;
+                    break;
+                }
+                currentHead = currentHead.next;
+            }
+            this.size++;
+
+        }
+        public void addNodeStart()
+        {
+
+            //Node newNode = new Node(GetName());
+            Node newNode = new Node("xzcz");
+            Node tmp = head;
+            newNode.next = head;
+            head = newNode;
+            this.size++;
+        }
+
+        public void add(string aValue)
+        {
+
+            Node newNode = new Node(aValue);
+            Node tmp = head;
+
+            newNode.next = head;
+            head = newNode;
+            this.size++;
+        }
+
+        public void addNodeAt(int pos, Node newNode)
+        {
+            Node curHead = head;
+
+            for (int i = 0; i < pos-1 ; i++)
+            {
+
+                curHead = curHead.next;
+            }
+
+            if (pos == 0)
+            {
+                Node temp = head;
+                head = newNode;
+                newNode.next = temp;
+            }
+            else
+            {
+                Node next = curHead.next;
+                curHead.next = newNode;
+                newNode.next = next;
+            }
+
+        }
+
+
+
+
+        public void removeNode(int pos)
+        {
+            if (pos == -1)
+            {
+                Console.WriteLine("Enter index to delete: ");
+                pos = int.Parse(Console.ReadLine());
+            }
+
+            //if valid index
+            if (pos >= 0 && pos <= this.size-1)
+            {
+                Node cur = head;
+                
+                for (int i = 0; i < pos - 1; i++)
+                {
+                    //Console.WriteLine("position: {0}", pos);
+                    //Console.Read();
+                    cur = cur.next;
+                }
+                
+                if(pos == 0)
+                {
+                    head = cur.next;
+                }
+
+
+                else if (cur.next.next != null)
+                {
+                    cur.next = cur.next.next;
+                    //head = cur.next;
+                }
+                else if (cur.next.next == null)
+                {
+                    cur.next = null;
+                }
+                this.size--;
+            }else
+            {
+                Console.WriteLine("Not a valid value");
+            }
+        }
+        
+       
+        public void PrintNodes()
+        {
+            Node cur = head;
+            int i = 0;
+            //Console.WriteLine("cur: {0}", cur.value);
+            while (cur != null)
+            {
+                if(cur.next == null)
+                {
+                    Console.WriteLine("{0}. {1} --> null", i, cur.value);
+                }else
+                {
+                    Console.Write("{0}. {1} -->", i, cur.value);
+                }
+                i++;
+                cur = cur.next;
+            }
+
+            Console.WriteLine("Size: {0}\n", this.size-1);
+        }
+    }
+}
